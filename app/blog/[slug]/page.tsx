0e1c165aab63da33b46bb8ce3dd1486feb679a82
Sakib Rhaman Opu <sakibrahaman65@gmail.com>
2025-06-02 23:42:32 +0600

    Add generateStaticParams function to support dynamic blog slugs

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,3 +1,4 @@
+// app/blog/[slug]/page.tsx
 import { notFound } from 'next/navigation';
 import BlogSinglePage from '../why-unit-conversion-matters/page';
 import BlogSinglePage2 from '../top-5-unit-conversion-tips/page';
@@ -8,12 +9,30 @@ const blogComponents: Record<string, React.FC> = {
   'top-5-unit-conversion-tips': BlogSinglePage2,
 };
 
+// --- Add this function ---
+export async function generateStaticParams() {
+  // Get all the keys (slugs) from your blogComponents map
+  const slugs = Object.keys(blogComponents);
+
+  // Map them to the format Next.js expects: [{ slug: '...' }, { slug: '...' }]
+  return slugs.map(slug => ({
+    slug: slug,
+  }));
+}
+// --- End of added function ---
+
 export default function BlogDynamicPage({
   params,
 }: {
   params: { slug: string };
 }) {
   const BlogComponent = blogComponents[params.slug];
-  if (!BlogComponent) return notFound();
+
+  // If the slug doesn't match any component, return 404
+  if (!BlogComponent) {
+    return notFound();
+  }
+
+  // Render the corresponding blog component
   return <BlogComponent />;
 }
